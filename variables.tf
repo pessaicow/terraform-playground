e6825c049f5f0ff16fcf35eb7e9aeec8017794ae
pessaicow <92889185+pessaicow@users.noreply.github.com>
2021-11-19 13:47:31 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ibmcloud_api_key" {}
+variable "region" {}
+
+#variable "solution_name" {
+#    description = "Suffix to be used while naming resources"
+#}
+
+#variable "direct_link_customer_name" {
+#    description = "Direct Link Customer name"
+#}
+
+#variable "address_range" {
+#    description = "Prefixes to be used for this core networking"
+#}
